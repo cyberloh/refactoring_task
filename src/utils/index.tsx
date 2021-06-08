19134d8863cb0ac5b544e9241bc595687c7ce2d7
cyberloh <=>
2021-06-09 00:43:29 +1000

    variable renamed

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -4,7 +4,7 @@ const randString = (stringLength: number):string => {
   return [...Array(stringLength)].map(i => (~~(Math.random()*36)).toString(36)).join('');
 };
 
-export const getDataPage = (startId: number, listLength:number, stringLength: number):DataItem[] => {
-  return Array.from({length: listLength}, (x, i) => i).map(id => ({id: id + startId, title: randString(stringLength)}));
+export const getDataPage = (startId: number, pageLength:number, stringLength: number):DataItem[] => {
+  return Array.from({length: pageLength}, (x, i) => i).map(id => ({id: id + startId, title: randString(stringLength)}));
 };
 
