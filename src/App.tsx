7a4590b44f2861df2b7b95592cfc88f3b38cf9ad
cyberloh <=>
2021-06-08 19:38:37 +1000

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -2,96 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const randString = ():string => {
+  return [...Array(10)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
+};
+
+const getDataPortion = (startId: number, length:number):any[] => {
+  return [...Array(length)].map(id => ({id: id + startId, title: randString()}));
+};
 
 function App() {
-    // d - List of items that can be updated with new additional items when the button is pressed
-    var [d, set] = React.useState()
+  
+  const dataPortionLength = 20; //imagine we've got it from user's settings
+  
+  const [data, setData] = React.useState(getDataPortion(0, dataPortionLength)); //when we name it 'data' it requires no explaination
 
-    var fill = () => {
-        // The function fills the list for render with some items
+  const dataRowsRender = ():any[] => {
+    return data.map(item => (
+      <div key={'data-row-' + item['id']} className={'App-item'}>{'Title is:' + item.title + '!'}</div>
+    ));
+  };
 
-        [...Array(20)].forEach((_, index) => {
-            if (!Array.isArray(d)) {
-                // @ts-ignore
-                d = []
-            }
+  const onAddMoreClick = (e:any)  => {
+    setData([...data, ...getDataPortion(data.length, dataPortionLength)]);
+  };
 
-            // @ts-ignore
-            d.push({
-                id: index, title: (function () {
-                    var result = [];
-                    for (var i = 0; i < 10; i++) {
-                        result.push('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.charAt(Math.floor(Math.random() *
-                            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.length)));
-                    }
-                    var string = result.join('');
-                    return string
-                })()
-            })
-        })
-        set(d)
-    }
-
-    // Fills the List onmount
-    fill()
-
-    var render = () => {
-        // renders the list of items as components
-
-        if (!Array.isArray(d)) {
-            // @ts-ignore
-            d = []
-        }
-        var result:any = []
-        // @ts-ignore
-        d.forEach(function (i, index) {
-            result.push(<div className={'App-item'}>{'Title is:' + i.title + '!'}</div>)
-        })
-        return result
-    }
-
-    return (
-        <div className="App">
-            <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-            </div>
-            <div>
-                <button className={"App-button"} onClick={() => {
-                    // The Function adds new items to the existing list
-
-                    if (!Array.isArray(d)) {
-                        // @ts-ignore
-                        d = []
-                    }
-                    [...Array(20)].forEach((_, index) => {
-                        if (!Array.isArray(d)) {
-                            // @ts-ignore
-                            d = []
-                        }
-                        // @ts-ignore
-                        d.push({
-                            id: index, title: (function () {
-                                var result = [];
-                                for (var i = 0; i < 10; i++) {
-                                    result.push('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.charAt(Math.floor(Math.random() *
-                                        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.length)));
-                                }
-                                var string = result.join('');
-                                return string
-                            })()
-                        })
-                    })
-                    // @ts-ignore
-                    set(d)
-                }}>
-                    Add More
-                </button>
-            </div>
-            <div>
-                {render()}
-            </div>
-        </div>
-    );
+  return (
+      <div className="App">
+          <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+          </div>
+          <div>
+              <button className={"App-button"} onClick={onAddMoreClick}>
+                  Add More
+              </button>
+          </div>
+          <div>
+              { dataRowsRender() }
+          </div>
+      </div>
+  );
 }
 
 export default App;
